de7e6a8ae0cccc3146ecc688f96565bce61a145a
Th3N4m3 <144971731+TTN-ATTN@users.noreply.github.com>
2024-05-17 11:15:24 +0700

    Delete ttn_dictionary directory

--- ttn_dictionary/main.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from app.app import app
-from fastapi.staticfiles import StaticFiles
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
